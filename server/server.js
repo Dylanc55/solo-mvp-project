9637f8161dbac1ec3ec05a55768a90aeb141a752
Dylan Cooper <dylanc55@Dylans-MacBook-Air.local>
2020-08-10 18:35:15 +0900

    trying to deploy to Heroku

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3000;
+const path = require("path");
+const publicPath = path.join(__dirname, "..", "public")
 const db = require("../db/knex");
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
+
+// Serve static assets
+app.use(express.static(publicPath));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,7 +22,12 @@ app.get("/api/recipes", async (req, res) => {
       console.error("Error loading recipes!", err);
       res.sendStatus(500);
     }
-  });
+});
+
+// Always return the main index.html, so react-router render the route in the client
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
