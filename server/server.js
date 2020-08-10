f648eb16e5efcc436c22015ac581266e9c9ea842
Dylan Cooper <dylanc55@Dylans-MacBook-Air.local>
2020-08-10 17:36:06 +0900

    endpoint to retrieve recipes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
+const db = require("../db/knex");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get("/api/recipes", async (req, res) => {
+    try {
+      const recipes = await db.select().table("recipes");
+      res.json(recipes);
+    } catch (err) {
+      console.error("Error loading recipes!", err);
+      res.sendStatus(500);
+    }
+  });
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
