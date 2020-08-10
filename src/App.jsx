c916c1044307368221049442b6b162da10919629
Dylan Cooper <dylanc55@Dylans-MacBook-Air.local>
2020-08-10 16:35:56 +0900

    added material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from "@material-ui/core";
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Recipe Book</header>
+      <Button variant="contained">Add Recipe</Button>
     </div>
   );
 }
