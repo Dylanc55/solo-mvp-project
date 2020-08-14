bd22340bf6d4964b23405b67d1caaa92c26324be
Dylan Cooper <dylanc55@Dylans-MacBook-Air.local>
2020-08-14 11:36:56 +0900

    added logo and changed favicon image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button } from "@material-ui/core";
 import './styles/App.css';
+import icon from "./img/app-icon.jpeg";
 import Form from "./components/Form";
 import FormDelete from "./components/FormDelete";
 import Display from "./components/Display";
@@ -9,7 +10,9 @@ function App() {
   const [storeRecipe, setStoreRecipe] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">Recipe Book</header>
+      <header className="App-header">
+        <img className="icon" src={icon} alt="icon"></img>
+      </header>
       <Button variant="contained" onClick={() => setStoreRecipe(!storeRecipe)}>Add Recipe</Button>
       {storeRecipe ? (
         <Form storeRecipe={storeRecipe} setStoreRecipe={setStoreRecipe} />
